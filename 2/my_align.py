5f74ad0a6259d2ef5eb2e131ed76c79066e76af9
Sachi Sanghavi <sachi@jhu.edu>
2016-02-26 18:28:58 -0500

    Update model
    Switch from using t(f|e) to to t(e|f) in IBM model 1.

--- 2/my_align.py
+++ 2/my_align.py
@@ -2,7 +2,7 @@
 
 ## This program is built on the align program written for CS468 MT @ JHU.
 ## The program implements the IBM Model 1 for French-English word alignments
-## using transition probabilities t(f|e) and most probable alignment.
+## using transition probabilities t(e|f) and most probable alignment.
 
 import optparse
 import sys
@@ -19,15 +19,15 @@ f_data = "%s.%s" % (opts.train, opts.french)
 e_data = "%s.%s" % (opts.train, opts.english)
 
 bitext = [[sentence.strip().split() for sentence in pair] for pair in zip(open(f_data), open(e_data))[:opts.num_sents]]
-e_count = defaultdict(float) # Maps an English word to expected count
+f_count = defaultdict(float) # Maps an English word to expected count
 fe_count = defaultdict(float) # Maps all (f,e) word pairs to expected count
-t_probability = defaultdict(float) # transition probability of f->e
+t_probability = defaultdict(float) # Transition probability
 
 sys.stderr.write("Initializing theta...")
 for (n, (f, e)) in enumerate(bitext):
     for f_i in set(f):
         for e_j in set(e):
-            t_probability[(f_i,e_j)] = 1.0/(len(e)+1) # uniform distribution
+            t_probability[(f_i,e_j)] = 1.0/(len(e)+1) # Uniform distribution
         if n % 500 == 0:
             sys.stderr.write(".") # To give user rough-estimate of execution progress
 sys.stderr.write("\n")
@@ -38,31 +38,31 @@ k = 0 # Number of iterations for EM loop
 while k < 5:
     k += 1
 
-    # Set count(f|e) = 0 for all (f,e) and set total(e) = 0 for all e
+    # Set count(e|f) = 0 for all (f,e) and set total(f) = 0 for all f
     for (n, (f, e)) in enumerate(bitext):
-        for e_j in set(e):
-            e_count[e_j] = 0
-            for f_i in set(f):
+        for f_i in set(f):
+            f_count[f_i] = 0
+            for e_j in set(e):
                 fe_count[(f_i,e_j)] = 0
         if n % 500 == 0:
             sys.stderr.write(".")
 
     # Expectation or E-step
     for (n, (f, e)) in enumerate(bitext):
-        for (i, f_i) in enumerate(f):
+        for (j, e_j) in enumerate(e):
             Z = 0 # Normalization term
-            for (j, e_j) in enumerate(e):
+            for (i, f_i) in enumerate(f):
                 Z += t_probability[(f_i, e_j)]
-            for (j, e_j) in enumerate(e):
+            for (i, f_i) in enumerate(f):
                 c = t_probability[(f_i, e_j)] / Z # Expected count
-                fe_count[(f_i, e_j)] += c # Increment count(f,e) by expected count
-                e_count[e_j] += c # Increment count(e) by expected count
+                fe_count[(f_i, e_j)] += c # Increment count(e|f) by expected count
+                f_count[f_i] += c # Increment count(f) by expected count
             if n % 5000 == 0:
                 sys.stderr.write(".")
 
     # Maximation or M-step
     for (f_i, e_j) in fe_count.keys():
-        t_probability[(f_i,e_j)] = fe_count[(f_i, e_j)] / e_count[e_j]
+        t_probability[(f_i,e_j)] = fe_count[(f_i, e_j)] / f_count[f_i]
 
 sys.stderr.write("\n")
 
